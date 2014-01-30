e516ce43f1f1c4300466aacbf1d0c4e06fa7dbaf
recorridosvirtuales <felipe@recorridosvirtuales.com>
2014-01-30 16:52:23 -0600

    Agregamos pleca
    modificación a xml y plega agregada

---
+++ Aeropano/Aeropano/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Aeropano
+//
+//  Created by Felipe Gonzalez on 29/01/14.
+//  Copyright (c) 2014 Felipe Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
